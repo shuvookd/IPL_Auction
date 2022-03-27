04c30da0ba33a562957478b8600c87f2dfc08fcc
shuvokd <shuvokd1210@gmail.com>
2022-03-27 20:56:51 +0600

    Q Ans Written

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -33,6 +33,28 @@ const Profile = () => {
             <div className="cart-container">
                 <Cart cart={cart} ></Cart>
             </div>
+
+            <h2>Question:How Re-act Works?
+                <br/>React is maintained by Facebook.
+                It is used for declarative paradigm 
+                that makes easier to reason of application 
+                and aims to be both efficient and flexible.
+                It is simple view of each state and it it
+                will efficiently update and render the right
+                component when data changes.<br/>Question:Difference
+                between Props VS State.<br/>
+                The man difference between props and state is
+                the state is internal and controlled by its component itself
+                and here props is external and controlled
+                by whatever renders the component.Another difference is
+                props is immutable and state is mutable.<br/>
+                Question:How use State works?<br/>
+                Use state is a function that allow state variable in
+                functional component.When we pass the initial state
+                to this function and it returns a variable with the
+                current state value it is not necessarily the initial state
+                and another function to update this value.</h2>
+            
         </div>
     );
 };
